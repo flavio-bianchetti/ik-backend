05c79f483d083c154855a617d27f2d0521744301
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-27 17:24:43 -0300

    refactor: dialect changed to mysql

--- src/config/database.ts
+++ src/config/database.ts
@@ -11,7 +11,7 @@ const Config: Options = {
   database: DB_DATABASE,
   host: DB_HOSTNAME,
   port: Number(DB_PORT) || 3306,
-  dialect: 'postgres',
+  dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
   },
